89d7eb0cef9ab35ebb9211eb96298bfeccc18da0
raysamoura <mouraraysa081@gmail.com>
2024-02-28 16:32:40 -0300

    importei as classes necessarias

--- src/main/java/org/example/MySql/ConnectorFactory.java
+++ src/main/java/org/example/MySql/ConnectorFactory.java
@@ -1,5 +1,10 @@
 package org.example.MySql;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class ConnectorFactory {
     public static void main(String[] args) {
 
