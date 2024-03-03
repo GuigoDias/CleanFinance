f14f203a8abaa1776ae0066aa756bfd8b4b37934
Guilherme <guilhermesdias.git@gmail.com>
2024-03-02 21:02:14 -0300

    Criação do ControleDeCadastro.java, correção do ContaDAO.java e ContaService.java e Teste pronto na Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,23 +1,13 @@
 package org.example;
 
-import org.example.MySql.ConnectionFactory;
+import org.example.ClassesPrincipais.Cliente;
 import org.example.TesteCliente.ContaService;
-
-import java.sql.SQLOutput;
-import java.util.Scanner;
+import org.example.TesteCliente.ControleDeCadastro;
 
 public class Main {
     private static ContaService service = new ContaService();
-    private static Scanner teclado = new Scanner(System.in);
     public static void main(String[] args) {
-
-        System.out.println("Digite o nome do cliente:");
-        String nome = teclado.toString();
-        System.out.println("Digite o cpf do cliente:");
-        int cpf = teclado.nextInt();
-        System.out.println("Digite o email do cliente:");
-        String email = teclado.toString();
-
-
+        Cliente cliente = ControleDeCadastro.controlandoDados();
+        service.criarConta(cliente);
     }
 }
--- src/main/java/org/example/TesteCliente/ContaService.java
+++ src/main/java/org/example/TesteCliente/ContaService.java
@@ -16,25 +16,7 @@ public class ContaService {
     }
 
     public void criarConta(Cliente cliente){
-        Cliente novoCliente = new Cliente(cliente.getNome(),cliente.getCpf(),cliente.getEmail());
-        String transformarCpf = String.valueOf(novoCliente.getCpf());
-
-        String sql = "INSERT INTO conta(saldo,cliente_nome,cliente_cpf,cliente_email,conta_ativa) VALUES (?,?,?,?,?)";
         Connection conn = connection.conectandoDB();
-
-        try {
-            PreparedStatement ps = conn.prepareStatement(sql);
-            ps.setBigDecimal(1, BigDecimal.ZERO);
-            ps.setString(2,novoCliente.getNome());
-            ps.setString(3,transformarCpf);
-            ps.setString(4, novoCliente.getEmail());
-            ps.setBoolean(5,true);
-
-            ps.execute();
-            ps.close();
-            conn.close();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        new ContaDAO(conn).criandoConta(cliente);
     }
 }
